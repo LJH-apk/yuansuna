e239e054dd229549c2e3b4bcce38c6886ca75f05
LJH-apk <18701059325@qq.com>
2020-04-05 21:02:07 +0800

    社团练习代码4.5

--- src/Main.java
+++ src/Main.java
@@ -9,9 +9,9 @@ public class Main {
         Scanner input = new Scanner(System.in);
         Number number = new Number();
         try{
-            System.out.println("");/** 提示语 */
+            System.out.println("请输入上一次考试的分数");/** 提示语 */
             number.number1 = input.nextFloat();/**   number.number1和number.number2是要计算的数字   */
-            System.out.println("");/** 提示语 */
+            System.out.println("请输入这场考试的分数");/** 提示语 */
             number.number2 = input.nextFloat();
         }
         catch (InputMismatchException e){
@@ -20,6 +20,8 @@ public class Main {
             System.exit(0);
         }
         //TODO 下面的关键运算代码交给你了
+        float number3 = ((number.number2-number.number1) / number.number1)*100;
+        System.out.println("你进步了：" + number3 + "%");
 
     }
 }
