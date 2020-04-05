0523e6263692290360779f997445e4f94f977ed8
LJH-apk <18701059325@qq.com>
2020-04-05 19:49:52 +0800

    社团练习代码2.0

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+/**
+ * @author 刘佳航
+ * @date 2020/4/5  19:39
+ */
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        Number number = new Number();
+        try{
+            System.out.println("");/** 提示语 */
+            number.number1 = input.nextFloat();/**   number.number1和number.number2是要计算的数字   */
+            System.out.println("");/** 提示语 */
+            number.number2 = input.nextFloat();
+        }
+        catch (InputMismatchException e){
+            System.out.println("Errer:你输入的不是数字");
+            System.out.println("处理完毕\n程序退出");
+            System.exit(0);
+        }
+        //TODO 下面的关键运算代码交给你了
+
+    }
+}
+class Number{
+    public float number1;
+    public float number2;
+}
